1a79baa1df52149ea458070061689ad166249db1
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2015-06-09 12:37:18 +0200

    Initial Commit

---
+++ Syncthing Helper/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Syncthing Helper
+//
+//  Created by Jelmer van der Linde on 09/06/15.
+//  Copyright (c) 2015 Jelmer van der Linde. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
